9f4675b932b1d1e2896eace5c1580f52748f44b4
datatong <59157188+datatong@users.noreply.github.com>
2021-07-10 21:21:28 -0700

    Add files via upload

---
+++ rps-functions.js
@@ -0,0 +1,10 @@
+function computerPlay() {
+    const computerRps = ["Rock", "Paper", "Scissors"];
+    var computerRandom = Math.floor(Math.random() * computerRps.length);
+    }
+
+function playRound(playerRps, computerRps) {
+    //Still must make playerRps case insensitive.
+    const computerRps = computerPlay();
+    const playerRps = input("Rock, paper, or scissors?");
+}
