39b5361401f55b985bac48d8f28d215e59489a0f
Anthony Tong <w.anthonytong@gmail.com>
2021-07-11 15:22:12 -0700

    Created conditions for playRound

--- rps-functions.js
+++ rps-functions.js
@@ -4,7 +4,34 @@ function computerPlay() {
     }
 
 function playRound(playerRps, computerRps) {
-    //Still must make playerRps case insensitive.
+    //Still must make playerRps input case-insensitive.
     const computerRps = computerPlay();
     const playerRps = input("Rock, paper, or scissors?");
+
+    if (playerRps = computerRps) {
+        return "It's draw game!";
+    }
+
+    if (playerRps = "Rock" && computerRps = "Scissors") {
+        return "You win! Rock beats scissors.";
+    }
+        else if (playerRps = "Paper" && computerRps = "Rock") {
+            return "You win! Paper beats rock.";
+        }
+        else if (playerRps = "Scissors" && computerRps = "Paper") {
+            return "You win! Scissors beats rock.";
+        }
+
+    if (playerRps = "Scissors" && computerRps = "Rock") {
+        return "You lose! Rock beats scissors.";
+    }
+        else if (playerRps = "Rock" && computerRps = "Paper") {
+            return "You lose! Paper beats rock.";
+        }
+        else if (playerRps = "Paper" && computerRps = "Scissors") {
+            return "You lose! Scissors beat paper.";
+        }
+        else {
+            alert("Invalid input. Try again.");
+        }
 }
