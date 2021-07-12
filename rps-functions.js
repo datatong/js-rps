7104b41e6c38807819899cd27d0a2305adea125b
Anthony Tong <w.anthonytong@gmail.com>
2021-07-11 18:43:50 -0700

    Added the computerPlay function call

--- rps-functions.js
+++ rps-functions.js
@@ -1,34 +1,37 @@
+const playerChoice = prompt("Rock, paper, or scissors?");
+const computerChoice = computerPlay();
+computerPlay();
+//playRound();
+
 function computerPlay() {
     const rpsRandom = ["Rock", "Paper", "Scissors"];
     const computerChoice = rpsRandom[Math.floor(Math.random() * rpsRandom.length)];
-    console.log(computerChoice);
-
+    //console.log(computerChoice);
+//}
 
     function playRound(playerChoice, computerChoice) {
-        /*var playerChoice = input("Rock, paper, or scissors?");
-        const computerChoice = computerPlay();*/
-
+        //Condition for draw games
         if (playerChoice = computerChoice) {
             return "It's a draw game!";
         }
         //Condition for user wins
-        if (playerchoice = "Rock" && computerChoice = "Scissors") {
+        if (playerchoice == "Rock" && computerChoice == "Scissors") {
             return "You win! Rock beats scissors.";
         }
-            else if (playerChoice = "Paper" && computerChoice = "Rock") {
+            else if (playerChoice == "Paper" && computerChoice == "Rock") {
                 return "You win! Paper beats rock.";
             }
-            else if (playerChoice = "Scissors" && computerChoice = "Paper") {
+            else if (playerChoice == "Scissors" && computerChoice == "Paper") {
                 return "You win! Scissors beats rock.";
             }
         //Condition for computer wins 
-        if (playerChoice = "Scissors" && computerChoice = "Rock") {
+        if (playerChoice == "Scissors" && computerChoice == "Rock") {
             return "You lose! Rock beats scissors.";
         }
-            else if (playerChoice = "Rock" && computerChoice = "Paper") {
+            else if (playerChoice == "Rock" && computerChoice == "Paper") {
                 return "You lose! Paper beats rock.";
             }
-            else if (playerChoice = "Paper" && computerChoice = "Scissors") {
+            else if (playerChoice == "Paper" && computerChoice == "Scissors") {
                 return "You lose! Scissors beat paper.";
             }
             else {
@@ -36,5 +39,3 @@ function computerPlay() {
             }
     }
 }
-playerChoice = input("Rock, paper, or scissors?");
-const computerChoice = computerPlay();
