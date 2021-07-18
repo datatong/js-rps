c9f3cfac24b40e0ecb468c6a1284207b613b9ae0
datatong <59157188+datatong@users.noreply.github.com>
2021-07-17 23:10:53 -0700

    Add files via upload

--- rps-functions.js
+++ rps-functions.js
@@ -1,27 +1,44 @@
 const playerChoice = prompt("Play 5 rounds! Rock, paper, or scissors?");
 playerChoice.toUpperCase();
 const computerChoice = computerPlay();
+
+/*Plays 5 rounds of RPS if I don't call playRound 5 times instead
+game();
+game();
+game();
+game();
+game();
+*/
+
 console.log(game());
+
 
 function computerPlay() {
     const rpsRandom = ["Rock", "Paper", "Scissors"];
     return rpsRandom[Math.floor(Math.random() * rpsRandom.length)];
-    //console.log(computerChoice);
+    /*Test
+    console.log(computerChoice);
+    */
 }
 
 function game() {
     var round = 0;
     var playerScore = 0;
     var computerScore = 0;
-    var tieGame= 0;
-    console.log(playRound());
+    var drawGame= 0;
+    //Play 5 rounds of RPS
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playround();
 
     function playRound() {
         //Condition for draw games
         if (playerChoice == computerChoice) {
-            return "It's a draw game!";
+            return "It's a draw!";
             round++;
-            tieGame++;
+            drawGame++;
         }
         //Condition for user wins
         if (playerChoice == "ROCK" && computerChoice == "Scissors") {
@@ -58,14 +75,24 @@ function game() {
             else {
                 alert("Invalid input. Try again.");
             }
-        
+
         return `Round ${round} completed!`;
         return "Player: " + playerScore + '\n'
                     "Computer: " + computerScore + '\n'
-                    "Tie: " + tieGame;        
+                    "Draw game(s): " + drawGame;
+        //Should I add a repeat function here?        
     }
 
     function alertFinalResult() {
-        if () {}
+        if (round == 5 && playerScore > computerScore) {
+            alert("You won the game!")
+        }
+        if (round == 5 && playerscore < computerScore) {
+            alert("You lost the game.")
+        }
+        if (round == 5 && playerscore = computerScore) {
+            alert("The game is a draw.")
+        }
     }
+    alertFinalResult()
 }
